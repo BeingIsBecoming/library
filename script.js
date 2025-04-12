0b44733319ddb2e01c30a28ab6df6303b65cf616
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-04-11 19:01:23 -0700

    Create basic structure and placeholder cards

---
+++ script.js
@@ -0,0 +1,24 @@
+// Library Web App
+
+// Array
+const myLibrary = ["VALIS", "Steppenwolf", ];
+
+// Book Constructor 
+function Book(title,author,genre,pages,status) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.pages = pages;
+  this.status = status;
+}
+
+// Add New Book to Array
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+//Push new book object to array 
+// const library = ["Banana", "Orange", "Apple", "Mango"];
+// library.push("Kiwi", "Lemon");
+
+// document.getElementById("demo").innerHTML = library;
