d57678a23d0bb6a899354d0228e2d04276c7960a
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-05-02 16:01:57 -0700

    Refactor script.

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-// Library Web App
+// Create Library Array
 const library = [];
 
-// Book Constructor 
-function Book(title,author,genre,pages,status) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.genre = genre;
-  this.pages = pages;
-  this.status = status;
+// Create Book Constructor
+function Book (title, author, genre, status) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.status = status;
+    this.id = crypto.randomUUID();
 }
 
 // Add Book to Array
-function addBook(title, author, genre, pages, status) {
-  const newBook = new Book(title, author, genre, pages, status);
-  library.push(newBook);
+function addBook(title,author,genre,status){
+    const newBook = new Book(title,author,genre,status);
+    library.push(newBook);
 }
+
+// Form Handler
+document.getElementById("form").addEventListener("submit", function(event){
+    // Prevent Page Reload
+    event.preventDefault();
+    // Get Form Values
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const genre = document.getElementById("genre").value;
+    const status = document.getElementById("status").value;
+    // Add New Book
+    addBook(title, author, genre, status);
+    // Clear Form Values
+    this.reset();
+});
