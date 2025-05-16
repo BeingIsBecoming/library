305d50e57936ab799462e6e4504e7220a3c6c5ad
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-05-15 18:09:31 -0700

    Script remove book button.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Create Library Array
+// Library Array
 const library = [
     new Book("VALIS","Philip K. Dick", "SciFi", "Read"),
-    new Book("Steppenwolf", "Herman Hesse", "SciFi", "Read"),
+    new Book("The Glass Bead Game", "Herman Hesse", "SciFi", "Read"),
     new Book("Be Here Now", "Ram Dass", "Spirituality", "Read"),
     new Book("Being & Time", "Martin Heidegger", "Philosophy", "Read"),
     new Book("The Voice of the Silence", "Madame Blavatsky", "Spirituality", "Read"),
@@ -18,18 +18,27 @@ function Book (title, author, genre, status) {
 
 // Display Book Cards 
 const cardsWrapper = document.querySelector(".cards-wrapper");
-function displayLibraryCards(){
+function displayLibraryCards() {
     cardsWrapper.innerHTML = "";
     library.forEach(book => {
         const card = document.createElement("div");
         card.className = "book-card";
+        card.setAttribute("data-id", book.id);
+
         card.innerHTML =
         `<h2 class = "title"> ${book.title}</h2>
         <h2 class = "author"> ${book.author}</h2>
         <p>Genre: ${book.genre}</p>
         <p>Status: ${book.status}</p>`;
-
         cardsWrapper.appendChild(card);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete";
+        deleteButton.innerText = "Remove";
+        deleteButton.addEventListener("click", ()=>{
+            removeBook(book.id);
+        });
+        card.appendChild(deleteButton);
     });
 }
 
@@ -40,8 +49,15 @@ function addBook(title,author,genre,status){
     displayLibraryCards();
 }
 
+// Remove Book from Library
+function removeBook(bookID) {
+    library.splice(0, library.length, ...library.filter
+        (book => book.id !== bookID));
+    displayLibraryCards();
+}
+
 // Form Handler
-document.getElementById("form").addEventListener("submit", function(event){
+document.getElementById("form").addEventListener("submit", (event)=> {
     // Prevent Page Reload
     event.preventDefault();
     // Get Form Values
